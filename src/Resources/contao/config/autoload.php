41b43546ecce01b2f9026e74c1f29d696d0868a8
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-06-09 14:54:48 -0400

    Create autoload.php

---
+++ src/Resources/contao/config/autoload.php
@@ -0,0 +1,7 @@
+<?php
+
+/* Register the classes */
+ClassLoader::addClasses(array
+(
+  'PinnedNews\Frontend\PinnedNews' 		=> 'system/modules/pinned_news/library/PinnedNews/Frontend/PinnedNews.php'
+));
