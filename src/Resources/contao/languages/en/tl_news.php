43f1f9cc63f34c645f6efc56fc481e4d6521ab8a
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-06-09 14:52:07 -0400

    Create tl_news.php

---
+++ src/Resources/contao/languages/en/tl_news.php
@@ -0,0 +1,7 @@
+<?php
+
+/* Fields */
+$GLOBALS['TL_LANG']['tl_news']['pinned'] 		= array('Pinned', 'Pin this news item.');
+
+/* Legends */
+$GLOBALS['TL_LANG']['tl_news']['pinned_legend'] 	= 'Pinned Posts';
