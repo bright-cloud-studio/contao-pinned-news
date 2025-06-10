13c9cc33e2aa7511110a03f5cd816f5b51391f98
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-06-10 08:56:13 -0400

    Update config.php

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -1,7 +1,9 @@
 <?php
 
+use Contao\Config;
+
 /* Cron */
-if (\Config::getInstance()->isComplete()) {
+if (Config::getInstance()->isComplete()) {
 	$GLOBALS['TL_HOOKS']['newsListFetchItems'] = array();
 	$GLOBALS['TL_HOOKS']['newsListFetchItems'][] = array('PinnedNews\Frontend\PinnedNews', 'fetchItems');
 }
